efbc5d48448849738dc2dadd4174b9dbfb92d2a2
amafjarkasi <amafjarkasi@gmail.com>
2021-02-03 15:35:46 +0000

    updates exercises

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,7 +1,12 @@
+import re
 
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 
 #your code go here:
+#list, dic
 
-print(hello)
+#new_list = [type(x) == "towel" for x in my_list]
+new_list = [i for i, n in my_list if str(type(n)) == "*list*"]
+
+print(new_list)
