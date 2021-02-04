c77d327d057db7d7e70d306737365b59b92d4284
amafjarkasi <amafjarkasi@gmail.com>
2021-02-04 12:17:28 +0000

    newest updates

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -2,4 +2,14 @@ arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
 
+sumTotal = 0
 
+def sumOdds():
+    for x in range(len(arr)):
+        print (int(x))
+        #if x % 2 != 0: sumTotal = sumTotal + x
+    #return sumTotal
+
+print(sumOdds)
+        
+        
